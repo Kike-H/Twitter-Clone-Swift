05c6689ab2ed6e1643a1aa48457ebe1c903d4893
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-10-15 13:20:51 -0500

    Initial Commit

---
+++ twitterclone/twittercloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twittercloneApp.swift
+//  twitterclone
+//
+//  Created by Kike Hernandez D. on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct twittercloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
